47f62b59e214cae1810ba14bb70611940d6cb35c
balazs21mate <58421674+balazs21mate@users.noreply.github.com>
2025-07-15 11:05:38 +0200

    Delete src/enviroments/enviroment.ts

--- src/enviroments/enviroment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  emailJSPublicKey: 'VI_sONHxMAPzXVVlW',
-  emailJSServiceId: 'service_48y0erl',
-  emailJSTemplateId: 'template_rxfpuqw',
-
-  minimumWage: 290800,
-  youthTaxCreditLimit: 559100,
-};
